ce22b8f6101d2b4c3630f8b4eda024c5c473ebc4
debbyprisca <deborahprisca8@gmail.com>
2025-04-21 20:49:00 +0300

    adds

---
+++ frontend/dashboard/src/components/ContentHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ContentHeader = () => {
+  return (
+    <div className='content--header'>
+        <h1 className="header--title">Dashboard</h1>
+        <div className="header--activity">
+            
+        </div>
+      
+
+    </div>
+  );
+};
+
+export default ContentHeader
